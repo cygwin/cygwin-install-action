be2e0c4e6d3c49a21602587e12e79ed1963116f5
Kurt McKee <contactme@kurtmckee.org>
2025-12-31 07:43:12 -0600

    Test splitting of the site list
    This introduces the following changes:
    
    * Test the code that splits and standardizes the `site` parameter.
    * Migrate from HTTP to HTTPS for the default sites.

---
+++ tests/Get-Validated-Sites.Tests.ps1
@@ -0,0 +1,30 @@
+BeforeAll {
+    . "$PSScriptRoot/../src/_functions.ps1"
+}
+
+Describe 'Get-Validated-Sites' {
+    Context 'Platform defaults' {
+        It 'Default (x86_64)' {
+            $sites = Get-Validated-Sites
+            Should -ActualValue $sites -Be @( 'https://mirrors.kernel.org/sourceware/cygwin/' )
+        }
+        It 'x86' {
+            $sites = Get-Validated-Sites -Platform 'x86'
+            Should -ActualValue $sites -Be @( 'https://mirrors.kernel.org/sourceware/cygwin-archive/20221123' )
+        }
+    }
+
+    Context 'Custom sites' {
+        It 'Single value' {
+            Get-Validated-Sites -Sites 'a' | Should -Be @( 'a' )
+        }
+
+        It 'Spaces' {
+            Get-Validated-Sites -Sites ' a b ' | Should -Be @( 'a', 'b' )
+        }
+
+        It 'Newlines and tabs' {
+            Get-Validated-Sites -Sites "`n`ta`n`tb`n`t" | Should -Be @( 'a', 'b' )
+        }
+    }
+}
