e50bbb7e3ed28bb2d623cc70e4c1fae797370ef0
Kurt McKee <contactme@kurtmckee.org>
2025-12-31 07:43:12 -0600

    Test splitting of the site list
    This introduces the following changes:
    
    * Test the code that splits and standardizes the `site` parameter.
    * Migrate from HTTP to HTTPS for the default sites.

--- src/_functions.ps1
+++ src/_functions.ps1
@@ -22,6 +22,24 @@ function Get-Validated-Platform {
 }
 
 
+function Get-Validated-Sites {
+    param (
+        $Platform,
+        $Sites
+    )
+
+    if ("$Sites" -eq '') {
+        switch ("$Platform") {
+            'x86'   { return @( 'https://mirrors.kernel.org/sourceware/cygwin-archive/20221123' ) }
+            # This is the default site for x86_64 platforms.
+            default { return @( 'https://mirrors.kernel.org/sourceware/cygwin/' ) }
+        }
+    }
+
+    return "$Sites" -Split '\s+' | Where-Object { $_ }
+}
+
+
 function Invoke-Cygwin-Setup {
     param (
         $SetupExePath,
